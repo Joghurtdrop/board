6d8ccbc87b2703ea45e63cc99a04cd9d90818be0
Hendrik Roehm <roehmhk@stud35.mathematik.uni-stuttgart.de>
2013-05-14 16:39:27 +0200

    LedPunkte auf Visualisierung der projektiven Ebene umgeändert

--- Virtualboard.py
+++ Virtualboard.py
@@ -5,7 +5,7 @@ from AbstractBoard import AbstractBoard
 class Virtualboard(AbstractBoard):
   def __init__(self):
     self.master = Tk()
-    self.w = Canvas(self.master, width=400, height=400)
+    self.w = Canvas(self.master, width=900, height=900)
     self.w.pack()
 
     self.i = 0
@@ -22,19 +22,30 @@ class Virtualboard(AbstractBoard):
     return s
 
   def circlesCoords(self):
+    c = list()
+    c.append((815.146, 319.083))
+    c.append((680.231, 644.798))
+    c.append((354.516, 779.713))
+    c.append(( 28.802, 644.798))
+    c.append((202.182, 471.417))
+    c.append((354.516, 496.249))
+    c.append((506.850, 471.417))
+    c.append((531.682, 319.083))
+    c.append((354.516, 319.083))
+    c.append((177.351, 319.083))
+    c.append((202.182, 166.749))
+    c.append((354.516, 141.918))
+    c.append((506.850, 166.749))
+
     self.c = list()
-    for i in range(5):
-      self.c.append((50*(i+1),       50,     50*(i+2),          100))
-    for i in range(5):
-      self.c.append((     300, 50*(i+1),          350,     50*(i+2)))
-    for i in range(5):
-      self.c.append((300-50*i,      300, 300-50*(i-1),          350))
-    for i in range(5):
-      self.c.append((      50, 300-50*i,          100, 300-50*(i-1)))
+    for i in c:
+      self.c.append((i[0], 900-i[1], i[0]+70.866, 900-i[1]-70.866))
 
   def drawCircles(self, cols):
-    self.w.create_rectangle(  0,   0, 400, 400, fill="white", width=0)
-    self.w.create_rectangle(100, 100, 300, 300, fill="blue", width=0)
+    self.w.create_rectangle(  0,   0, 900, 900, fill="white", width=0)
+    
+    #for j in range(len(self.c)):
+    #  self.w.create_oval(self.c[j][0], self.c[j][1], self.c[j][2], self.c[j][3], fill="#000", width=0)
     
     for i in range(min(len(self.c), len(cols))):
       j = (i+self.i) % len(self.c)
@@ -44,7 +55,7 @@ class Virtualboard(AbstractBoard):
     self.drawCircles(self.getGetterClass().get())
 
   def update(self):
-    self.i -= 1
+    self.i += 1
 
     self.refresh()
 
